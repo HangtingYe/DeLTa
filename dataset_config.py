027a53b43bb419323655940a2eb925748911b80e
Jinmeng Li <155933838+lijm-Au@users.noreply.github.com>
2025-10-10 15:31:52 +0800

    Add files via upload

---
+++ dataset_config.py
@@ -0,0 +1,106 @@
+"""Dataset configuration parameters for model training"""
+
+# Dataset-specific parameters
+dataset_params = {
+    'adult': {
+        'mds': [20],
+        'mls': [50],
+        'n_estimators': [15],
+        'small_model': 'cart', 
+        'eta':0.2,
+        'num_class': 2  # Binary classification
+    },
+    'bank': {
+        'mds': [5],
+        'mls': [50],
+        'n_estimators': [15],
+        'small_model': 'cart', 
+        'eta':0.2,
+        'num_class': 2  # Binary classification
+    },
+    'car': {
+        'mds': [20],
+        'mls': [20],
+        'n_estimators': [20],
+        'small_model': 'cart', 
+        'eta':0.07,  
+        'num_class': 4  # Regression
+    },
+    'blood': {
+        'mds': [10],
+        'mls': [10],
+        'n_estimators': [3],
+        'small_model': 'cart', 
+        'eta':0.028, 
+        'num_class': 2  # Regression
+    },
+    'credit-g': {
+        'mds': [15],
+        'mls': [20],
+        'n_estimators': [20],
+        'small_model': 'cart',  
+        'eta':0.1,
+        'num_class': 2  # Regression
+    },
+    'jannis': {
+        'mds': [20],
+        'mls': [50],
+        'n_estimators': [20],
+        'small_model': 'cart', 
+        'eta':0.2, 
+        'num_class': 4  # Regression
+    },
+    'cpu_act': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [3],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    'house_16H_reg': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [5],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    'credit_reg': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [3],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    'fried': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [5],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    'Diamonds': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [3],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    'california_housing': {
+        'mds': [10],
+        'mls': [200],
+        'n_estimators': [5],
+        'small_model': 'tabpfn',  
+        'num_class': 1  # Regression
+    },
+    
+}
+
+# Default parameters for datasets not explicitly defined
+default_params = {
+    'mds': [20],
+    'mls': [50],
+    'n_estimators': [20],
+    'small_model': 'cart',  
+    'num_class': 2  
+}
+    
