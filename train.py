09b1b1d55be3d320465e0ef264c14fc1493aa357
Jinmeng Li <155933838+lijm-Au@users.noreply.github.com>
2025-12-25 16:58:10 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,35 @@
+from tqdm import tqdm
+from model.utils import (
+    get_classical_args,tune_hyper_parameters,
+    show_results_classical,get_method,set_seeds
+)
+from model.lib.data import (
+    get_dataset
+)
+import matplotlib.pyplot as plt
+import argparse
+import pandas as pd
+import time
+import re
+
+if __name__ == '__main__':
+    results_list, time_list = [], []
+    args,default_para,opt_space = get_classical_args()
+    args.dataset_path = 'DeLTa-main/example_datasets'
+    train_val_data,test_data,info = get_dataset(args.dataset,args.dataset_path)
+    if args.tune:
+        args = tune_hyper_parameters(args,opt_space,train_val_data,info)
+    
+    ## Training Stage over different random seeds
+    for seed in tqdm(range(args.seed_num)):
+        args.seed = seed    # update seed  
+        set_seeds(args.seed)
+        method = get_method(args.model_type)(args, info['task_type'] == 'regression')
+        start_train = time.time()
+        time_cost = method.fit(train_val_data, info,train=True)   
+        end_train = time.time()
+        train_time = end_train - start_train
+
+        start_test = time.time()
+        method.predict(test_data, info, model_name=args.evaluate_option)
+        end_test = time.time()
